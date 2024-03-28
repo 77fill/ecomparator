b23f8989adb5e0a9a3b112d9c982163673998ad1
Philipp Schmalz <77fill@googlemail.com>
2024-03-28 19:07:28 +0100

    core: unit: several made-up units

--- core/src/main/java/com/example/ecomparator/core/Unit.kt
+++ core/src/main/java/com/example/ecomparator/core/Unit.kt
@@ -1,6 +1,9 @@
 package com.example.ecomparator.core
 
+import java.lang.IllegalStateException
 import java.math.BigDecimal
+
+val baseUnitOrder = listOf("m","s","kg","A","K","mol","cd")
 
 class Unit(
     val m: Int = 0,
@@ -12,6 +15,16 @@ class Unit(
     val cd: Int = 0,
     val prefix: BigDecimal = 1.toBigDecimal()
 ) {
+    val baseUnitExponents = mapOf(
+        "m" to m,
+        "s" to s,
+        "kg" to kg,
+        "A" to A,
+        "K" to K,
+        "mol" to mol,
+        "cd" to cd
+    )
+
     companion object {
 
     }
@@ -20,7 +33,18 @@ class Unit(
     val coherent = prefix == 1.toBigDecimal()
 
     override fun toString(): String {
-        return ""
+        return baseUnitOrder
+            .map { it to baseUnitExponents[it] }
+            .map {
+                when (it.second) {
+                    1 -> it.first
+                    0 -> ""
+                    null -> throw IllegalStateException("baseUnitExponents must have baseUnitOrder elements as keys!")
+                    else -> it.first + "^" + it.second
+                }
+            }
+            .filter {it != ""}
+            .joinToString("*")
     }
-    val fullForm = "1"
+    val fullForm = if(toString()=="") "1" else toString()
 }
--- core/src/test/java/com/example/ecomparator/core/UnitTest.kt
+++ core/src/test/java/com/example/ecomparator/core/UnitTest.kt
@@ -12,4 +12,21 @@ class UnitTest {
         Assert.assertTrue(unit.toString() == "")
         Assert.assertTrue(unit.fullForm == "1")
     }
+
+    @Test
+    fun several_made_up_units() {
+        var unit: Unit
+
+        unit = Unit(s=1,m=-2,K=2)
+        Assert.assertTrue(unit.baseUnitComposition == listOf(-2,1,0,0,2,0,0))
+        Assert.assertTrue(unit.coherent && unit.prefix == 1.toBigDecimal())
+        Assert.assertTrue(unit.toString() == "m^-2*s*K^2")
+        Assert.assertTrue(unit.toString() == unit.fullForm)
+
+        unit = Unit(prefix = 200_000.2.toBigDecimal(), kg=1, m=1, s=-2)
+        Assert.assertTrue(unit.baseUnitComposition == listOf(1,-2,1,0,0,0,0))
+        Assert.assertTrue(!unit.coherent && unit.prefix == 200_000.2.toBigDecimal())
+        Assert.assertTrue(unit.toString() == "m*s^-2*kg")
+        Assert.assertTrue(unit.toString() == unit.fullForm)
+    }
 }
